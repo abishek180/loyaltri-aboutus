5b5dd3f941590c33aecd0dc20f4a34307ec3a2a5
abishek180 <anilabishek15@gmail.com>
2024-06-19 11:00:33 +0530

    minor change

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,13 +5,12 @@ import { IoIosCheckmarkCircle } from "react-icons/io";
 const Footer = () => {
   return (
     <div>
-      <div className="flex flex-col justify-center items-center gap-5 bg-white h-auto lg:h-[465px] w-[calc(100%_-_10%)] mx-auto relative bottom-[130px] rounded-[16px] z-30 p-[50px]">
+      <div className="flex flex-col justify-center items-center gap-5 bg-white h-auto lg:h-[465px] w-[calc(100%_-_10%)] mx-auto relative bottom-[130px] rounded-[16px] z-30  p-8 lg:p-12">
         <div className="flex flex-col justify-center items-center gap-8">
-         
             <p className="w-full lg:w-[552px] text-black text-3xl md:text-6xl font-semibold text-center">
               Want to learn more about Loyaltri?
             </p>
-            <p className="w-full lg:[1000px] text-[#667085] text-2xl font-medium text-center">
+            <p className="w-full lg:[1000px] text-[#667085] text-xl md:text-2xl font-medium text-center">
               WWe'd love to chat with you more about how Loyaltri can support
               your business's HR needs. It's like having HR experts at your
               fingertips, just a click away!
@@ -20,9 +19,9 @@ const Footer = () => {
               <Link href="/contact">Book your Demo</Link>
             </button>
             <div className="flex flex-col md:flex-row justify-center items-center gap-10">
-                <p className="text-[#667085] text-sm font-normal flex items-center gap-2"><IoIosCheckmarkCircle className="text-black"/>A no-commitment <span className="text-black font-medium">product walkthrough</span></p>
-                <p className="text-[#667085] text-sm font-normal flex items-center gap-2"><IoIosCheckmarkCircle className="text-black"/><span className="text-black font-medium">Free Trial,</span> Cancel any time</p>
-                <p className="text-[#667085] text-sm font-normal flex items-center gap-2"><IoIosCheckmarkCircle className="text-black"/>Your questions,<span className="text-black font-medium">answered</span></p>
+                <p className="text-[#667085] text-xs md:text-sm font-normal flex items-center gap-2"><IoIosCheckmarkCircle className="text-black"/>A no-commitment <span className="text-black font-medium">product walkthrough</span></p>
+                <p className="text-[#667085] text-xs md:text-sm font-normal flex items-center gap-2"><IoIosCheckmarkCircle className="text-black"/><span className="text-black font-medium">Free Trial,</span> Cancel any time</p>
+                <p className="text-[#667085] text-xs md:text-sm font-normal flex items-center gap-2"><IoIosCheckmarkCircle className="text-black"/>Your questions,<span className="text-black font-medium">answered</span></p>
             </div>
         </div>
       </div>
--- components/Hr.jsx
+++ components/Hr.jsx
@@ -3,41 +3,64 @@ import { motion, useInView } from "framer-motion";
 import Image from "next/image";
 import React, { useRef } from "react";
 // images
-import hr from "@/public/images/meeting.png"
-import question from "@/public/images/question.png"
+import hr from "@/public/images/meeting.png";
+import question from "@/public/images/question.png";
+import pop from "@/public/images/pop-up.png";
 // components
 import DrawSvg from "./DrawSvg";
 import { GetChars } from "./GetChars";
 
 export const Hr = () => {
-      // slideup-text
+  // slideup-text
   const title = useRef(null);
+  const popImageRef = useRef(null);
   const isInView = useInView(title, { margin: "1%" });
+  const isPopInView = useInView(popImageRef, { once: true });
+
   return (
     <section className="max-w-screen-xl mx-auto px-5 lg:px-10 2xl:px-0 py-20">
       <div className="flex justify-center items-center relative">
         <div className="flex flex-col justify-center items-center gap-20">
-        <DrawSvg/>
-        <p className="text-white text-2xl md:text-[38px] text-center font-semibold leading-[122%] flex flex-col md:flex-row items-center gap-3">The constant question <span className="overflow-hidden w-[37px]"><Image src={question} className="w-full h-full object-cover object-center"/></span> we faced then was...</p>
-        <motion.div className="flex items-center justify-center gap-2">
-        <p className="text-2xl md:text-7xl text-gradient font-semibold ">Convenience <span className="text-white">is</span></p>
-          <div
-            ref={title}
-            className="text-2xl md:text-7xl text-white font-semibold text-center"
+          <DrawSvg />
+          <p className="text-white text-2xl md:text-[38px] text-center font-semibold leading-[122%] flex flex-col md:flex-row items-center gap-3">
+            The constant question{" "}
+            <span className="overflow-hidden w-[37px]">
+              <Image
+                src={question}
+                className="w-full h-full object-cover object-center"
+              />
+            </span>{" "}
+            we faced then was...
+          </p>
+          <motion.div
+            ref={popImageRef}
+            initial={{ scale: 1 }}
+            animate={{ scale: isPopInView ? 1.2 : 1 }}
+            transition={{ duration: 0.5 }}
+            className="w-full lg:w-[750px]"
           >
-            <GetChars
-              word="key"
-              isInView={isInView}
-            />
-          </div>
-        </motion.div>
-        <DrawSvg/>
-          <p className="text-white text-2xl md:text-[38px] text-center font-semibold leading-[122%] w-full lg:w-[922px]">Based on insights from interviews with 100 HR professionals, we refined our approach with input from our team to innovate further.</p>
+            <Image src={pop} className="w-full h-full object-cover object-center" />
+          </motion.div>
+          <motion.div className="flex items-center justify-center gap-2">
+            <p className="text-2xl md:text-7xl text-gradient font-semibold ">
+              Convenience <span className="text-white">is</span>
+            </p>
+            <div
+              ref={title}
+              className="text-2xl md:text-7xl text-white font-semibold text-center"
+            >
+              <GetChars word="key" isInView={isInView} />
+            </div>
+          </motion.div>
+          <DrawSvg />
+          <p className="text-white text-2xl md:text-[38px] text-center font-semibold leading-[122%] w-full lg:w-[922px]">
+            Based on insights from interviews with 100 HR professionals, we refined our approach with input from our team to innovate further.
+          </p>
           <div className="overflow-hidden w-full lg:w-[750px]">
-            <Image src={hr} className="w-full h-full object-cover object-center"/>
+            <Image src={hr} className="w-full h-full object-cover object-center" />
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
