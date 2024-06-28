4df84d763868eb8abe50328a4d4c977e8dbb7dcc
abishek180 <anilabishek15@gmail.com>
2024-06-28 16:17:56 +0530

    changes animation in page transition

--- utils/animations.js
+++ utils/animations.js
@@ -4,16 +4,21 @@ export const animatePageIn = () => {
   const bannerOne = document.getElementById("banner-1");
 
   if (bannerOne) {
-    gsap.fromTo(
+    const tl = gsap.timeline();
+    tl.fromTo(
       bannerOne,
       { yPercent: 0 },
       {
         yPercent: 100,
-        duration: 1,
-        stagger: 0.2,
+        duration: 1.5,
         ease: "power3.out",
       }
-    );
+    ).to(bannerOne, {
+      yPercent: 100,
+      duration: 1.5,
+      ease: "power3.out",
+      delay: 0,
+    });
   }
 };
 
@@ -21,15 +26,21 @@ export const animatePageOut = (href, router) => {
   const bannerOne = document.getElementById("banner-1");
 
   if (bannerOne) {
-    gsap.to(bannerOne, {
-      yPercent: -100,
-      duration: 2.5,
-      stagger: 0.2,
+    const tl = gsap.timeline();
+    tl.to(bannerOne, {
+      yPercent: -0,
+      duration: 1.5,
       ease: "power3.in",
-      onComplete: () => {
-        router.push(href);
-      },
-    });
+    })
+      .to(bannerOne, {
+        yPercent: -100,
+        duration: 1.5,
+        ease: "power3.in",
+        delay: 1,
+        onComplete: () => {
+          router.push(href);
+        },
+      });
   } else {
     router.push(href); // Fallback to navigate to the next page
   }
